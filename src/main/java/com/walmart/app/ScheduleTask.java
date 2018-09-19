8890bf23e29e5343e2b4e967d613383af5d8be06
radhaKannan <radhakannan@live.com>
2018-09-18 19:01:05 -0700

    removing seat hold id from schedule task

--- src/main/java/com/walmart/app/ScheduleTask.java
+++ src/main/java/com/walmart/app/ScheduleTask.java
@@ -6,14 +6,12 @@ import java.util.TimerTask;
 import java.util.TreeMap;
 
 public class ScheduleTask extends TimerTask {
-    private int seatHoldId;
     private SeatHold seatsInfo;
     private Map<Integer,SeatHold> heldTickets;
     private Map<Integer, List<List<Integer>>> rowSpaceMap;
     private TreeMap<Integer, List<Integer>> continuousSpaceMap;
 
     public ScheduleTask(int seatHoldId, Map<Integer,SeatHold> heldTickets, Map<Integer, List<List<Integer>>> rowSpaceMap, TreeMap<Integer, List<Integer>> continuousSpaceMap) {
-        this.seatHoldId = seatHoldId;
         this.heldTickets = heldTickets;
         this.rowSpaceMap = rowSpaceMap;
         this.continuousSpaceMap = continuousSpaceMap;
